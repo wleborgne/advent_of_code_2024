097c78bd5f81fd405c005e4e4d4cf246292d323e
Bill LeBorgne <bill@leborgne.name>
2024-12-02 12:55:48 -0700

    Refactoring of day 1
    Start of refactoring
    Wrap existing code in an actual class and give it a Main method
    Do some simple parsing of command line args to specify which day
    and whether or not to use sample code
    Next steps:
    - migrate the day 1 solution to its own method (and eventually
    its own file/class)
    - use command line arg to specify not just which file, but which
    method to call

--- Program.cs
+++ Program.cs
@@ -1,42 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
+namespace AdventOfCode;
 
 using System.Runtime.InteropServices;
 
-var cwd = System.IO.Directory.GetCurrentDirectory();
-// var dataFile = Path.Combine(cwd) + "/data/day1.sample.txt";
-var dataFile = Path.Combine(cwd) + "/data/day1.txt";
-Console.WriteLine($"Data file: {dataFile}");
-var sr = new StreamReader(dataFile);
-
-var contents = sr.ReadToEnd();
-var lines = contents.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
-Console.WriteLine(lines.Length);
-
-int[] left = new int[lines.Length];
-int[] right = new int[lines.Length];
-
-for(int i = 0; i < lines.Length; i++)
+class Program
 {
-    var foo = lines[i].Split(new string[] { "   " }, StringSplitOptions.RemoveEmptyEntries);
-    left[i] = Int32.Parse(foo[0]);
-    right[i] = Int32.Parse(foo[1]);
+    static void Main(string[] args)
+    {
+        string filename;
+        string dayNumber = args[0];
+        string sample = args[1];
+
+        if (sample == "true")
+        {
+            filename = $"/data/day{dayNumber}.sample.txt";
+        }
+        else
+        {
+            filename = $"/data/day{dayNumber}.txt";
+        }
+
+        var cwd = System.IO.Directory.GetCurrentDirectory();
+        var dataFile = Path.Combine(cwd) + filename;
+
+        Console.WriteLine($"Using data file: {dataFile}");
+        var sr = new StreamReader(dataFile);
+        var contents = sr.ReadToEnd();
+
+        var lines = contents.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        Console.WriteLine($"Read {lines.Length} lines.");
+
+        int[] left = new int[lines.Length];
+        int[] right = new int[lines.Length];
+
+        for(int i = 0; i < lines.Length; i++)
+        {
+            var foo = lines[i].Split(new string[] { "   " }, StringSplitOptions.RemoveEmptyEntries);
+            left[i] = Int32.Parse(foo[0]);
+            right[i] = Int32.Parse(foo[1]);
+        }
+
+        Array.Sort(left);
+        Array.Sort(right);
+
+        var total = 0;
+        for(int i = 0; i < lines.Length; i++)
+        {
+            total += Math.Abs(left[i] - right[i]);
+        }
+
+        Console.WriteLine($@"Part 1: {total}");
+
+        total = 0;
+        foreach(int leftVal in left) {
+            var results = Array.FindAll(right, (x) => x == leftVal);
+            total += leftVal * results.Count();
+        }
+
+        Console.WriteLine($@"Part 2: {total}");
+    }
 }
-
-Array.Sort(left);
-Array.Sort(right);
-
-var total = 0;
-for(int i = 0; i < lines.Length; i++)
-{
-    total += Math.Abs(left[i] - right[i]);
-}
-
-Console.WriteLine($@"Part 1: {total}");
-
-total = 0;
-foreach(int leftVal in left) {
-    var results = Array.FindAll(right, (x) => x == leftVal);
-    total += leftVal * results.Count();
-}
-
-Console.WriteLine($@"Part 2: {total}");
