ca69454999bfca7164a1c78ac74c47677d3a8255
Bill LeBorgne <bill@leborgne.name>
2024-12-02 09:27:51 -0600

    Day 1, Part 1
    Needs _massive_ refactoring, but it works (for now)
    First "real" C# console app, so I've got that going for me
    as well.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.InteropServices;
+
+var cwd = System.IO.Directory.GetCurrentDirectory();
+// var dataFile = Path.Combine(cwd) + "/data/day1.sample.txt";
+var dataFile = Path.Combine(cwd) + "/data/day1.txt";
+Console.WriteLine($"Data file: {dataFile}");
+var sr = new StreamReader(dataFile);
+
+var contents = sr.ReadToEnd();
+var lines = contents.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine(lines.Length);
+
+int[] left = new int[lines.Length];
+int[] right = new int[lines.Length];
+
+for(int i = 0; i < lines.Length; i++)
+{
+    var foo = lines[i].Split(new string[] { "   " }, StringSplitOptions.RemoveEmptyEntries);
+    left[i] = Int32.Parse(foo[0]);
+    right[i] = Int32.Parse(foo[1]);
+}
+
+Array.Sort(left);
+Array.Sort(right);
+
+var total = 0;
+for(int i = 0; i < lines.Length; i++)
+{
+    total += Math.Abs(left[i] - right[i]);
+}
+
+Console.WriteLine($@"Total: {total}");
