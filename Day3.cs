d39b3e406bba9b550cccc865bdf794997bb73f57
Bill LeBorgne <bill@leborgne.name>
2024-12-03 08:40:24 -0700

    Day 3, part 1
    Skipping day 2 part 2 for now; it just looks annoying.
    I'm probably not really using regex in the best way, but
    it works for now.

---
+++ Day3.cs
@@ -0,0 +1,40 @@
+using System.Text.RegularExpressions;
+
+namespace Advent;
+
+public class Day3
+{
+  private readonly string rawInput;
+  private readonly string[] lines;
+  private int total;
+  public Day3(string rawInput)
+  {
+    this.rawInput = rawInput;
+    lines = rawInput.Split("\n");
+    Console.WriteLine($"Read {lines.Length} lines.");
+  }
+
+  public int Part1()
+  {
+    total = 0;
+    var regex = @"mul\(\d{1,3},\d{1,3}\)";
+
+    foreach (var line in lines)
+    {
+      var matches = Regex.Matches(line, regex).ToArray();
+      foreach (var match in matches)
+      {
+        var nums = match.ToString().Substring(4, match.Length - 5).Split(',');
+
+        total += Int32.Parse(nums[0]) * Int32.Parse(nums[1]);
+      }
+    }
+    return total;
+  }
+
+  public int Part2()
+  {
+    total = 0;
+    return -1;
+  }
+}
